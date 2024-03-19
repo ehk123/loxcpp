bdbdda87f669f802d6e48f57587515c8e0b9b30a
Ethan Kim <ehk003@ucsd.edu>
2024-03-18 21:07:19 -0700

    change tab indent to 4 in .clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 
 int main() {
-  std::array<int, 5> my_array = {1, 52, 2, 4, 9};
-  std::sort(my_array.begin(), my_array.end());
+    std::array<int, 5> my_array = {1, 52, 2, 4, 9};
+    std::sort(my_array.begin(), my_array.end());
 
-  for (auto value : my_array) {
-    std::cout << value << ' ';
-  }
-  std::cout << '\n';
-  return 0;
+    for (auto value : my_array) {
+        std::cout << value << ' ';
+    }
+    std::cout << '\n';
+    return 0;
 }
